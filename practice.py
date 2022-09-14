cb0d8d86ce1ef0bc838f15bed3f44b9116f3efdc
Maksim <easypeasylemonskvii@gmail.com>
2022-09-15 00:44:21 +0300

    pactice #3

--- practice.py
+++ practice.py
@@ -1,38 +1,73 @@
 #1
-name = input('Please input your name: ')
-age = int(input('Please input your age: '))
+# name = input('Please input your name: ')
+# age = int(input('Please input your age: '))
 
-def print_user_info(name, age):
-    print('Hi ', name, ' Your age is: ', age)
-
-
-def decades_calc(age):
-  print(age // 10)
+# def print_user_info(name, age):
+#     print('Hi ', name, ' Your age is: ', age)
 
 
-print_user_info(name, age);
-
-decades_calc(age)
-
-print('-' * 20)
-
-#2
-
-name_list = ['Aleksei', 'Ben', 'Natasha', 'Alex']
-
-def get_names_length(list):
-    for name in list:
-      if len(name) > 5 and ('n' in name or 'N' in name):
-        print(name, len(name))
+# def decades_calc(age):
+#   print(age // 10)
 
 
+# print_user_info(name, age);
 
-def clear_list(list): 
-    while len(list) != 0:
-      list.pop()
-    print(list)
+# decades_calc(age)
+
+# print('-' * 20)
+
+# #2
+
+# name_list = ['Aleksei', 'Ben', 'Natasha', 'Alex']
+
+# def get_names_length(list):
+#     for name in list:
+#       if len(name) > 5 and ('n' in name or 'N' in name):
+#         print(name, len(name))
 
 
-get_names_length(name_list)
-clear_list(name_list)
 
+# def clear_list(list): 
+#     while len(list) != 0:
+#       list.pop()
+#     print(list)
+
+
+# get_names_length(name_list)
+# clear_list(name_list)
+
+
+#3
+
+person_list = [
+    {
+      'name': 'Max',
+      'age': 29,
+      'hobbies': 'cycling'
+    },
+    {
+      'name': 'Anna',
+      'age': 13,
+      'hobbies': 'coocking'
+    },
+     {
+      'name': 'Bob',
+      'age': 21,
+      'hobbies': 'workout'
+    },
+  ]
+
+names_list = [name["name"] for name in person_list]
+print(names_list)
+
+print(all(person["age"] > 20 for person in person_list))
+
+copy_person_list = [person.copy() for person in person_list]
+copy_person_list[0]["name"] = 'Vlad'
+print(copy_person_list)
+print(person_list)
+
+a,b,c = person_list
+print(a)
+print(b)
+print(c)
