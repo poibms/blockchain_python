e0e87994e88353991f74d1ab30c75d48f111da0f
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2022-09-24 15:34:51 +0300

    get open transactions

--- node.py
+++ node.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from wallet import Wallet
@@ -97,6 +97,57 @@ def load_keys():
             'message': 'Loading the keys failed'
         }
         return jsonify(response), 500
+
+
+@app.route('/transaction', methods=["POST"])
+def add_transaction():
+    if wallet.public_key is None:
+        resource = {
+            'message': 'No wallet set up'
+        }
+        return jsonify(resource), 400
+    values = request.get_json()
+    if not values:
+        resource = {
+            'message': 'No data found'
+        }
+        return jsonify(resource), 400
+    required_fields = ['recipient', 'amount']
+    if not all(field in values for field in required_fields):
+        response = {
+            'message': 'Required field is missing'
+        }
+        return jsonify(response), 400
+    recipient = values['recipient']
+    amount = values['amount']
+    signature = wallet.sign_transaction(wallet.public_key, recipient, amount)
+    success = blockchain.add_transaction(recipient, wallet.public_key, signature, amount)
+    if success:
+        response = {
+            'message': 'Successfully added transaction',
+            'transaction': {
+                'sender': wallet.public_key,
+                'recipient': recipient,
+                'amount': amount,
+                'signature': signature
+            },
+            'funds': blockchain.get_balance()
+        }
+        return jsonify(response), 201
+
+        return jsonify(response), 500
+    else:
+        response = {
+            'message': 'Creating a transaction failed'
+        }
+        return jsonify(response), 500
+
+
+@app.route('/transactions', methods=['GET'])
+def get_open_transactions():
+    transactions = blockchain.get_open_transactions()
+    dict_transactions = [tx.__dict__ for tx in transactions]
+    return jsonify(dict_transactions), 200
 
 
 if __name__ == '__main__':
